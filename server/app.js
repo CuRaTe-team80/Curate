c6cefa6d2ad36f3525722cab6382716a17df1de1
IshiniAluthwatta <aihaluthwatta@students.nsbm.ac.lk>
2026-08-15 22:23:50 +0530

    Update app routes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const app = express();
+
 app.use(express.json());
-app.get('/', (req, res) => res.send('Curate API running'));
+
+// Samples routes
+const samplesRouter = require('./routes/samples');
+app.use('/samples', samplesRouter);
+
+// Authentication routes
+const authRouter = require('./routes/auth');
+app.use('/auth', authRouter);
+
+// Root route
+app.get('/', (req, res) => {
+  res.send('Curate API running');
+});
+
 module.exports = app;
