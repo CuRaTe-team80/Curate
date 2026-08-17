33e56e4dd3305e4db5b473d976e6f3cfc7d65e3a
IsiwaraKumarage8 <isiwarakumarage6@gmail.com>
2026-08-18 00:55:41 +0530

    fix: add CORS middleware to allow client requests

--- server/app.js
+++ server/app.js
@@ -1,19 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
-// Samples routes
 const samplesRouter = require('./routes/samples');
 app.use('/samples', samplesRouter);
 
-// Authentication routes
-const authRouter = require('./routes/auth');
-app.use('/auth', authRouter);
-
-// Root route
-app.get('/', (req, res) => {
-  res.send('Curate API running');
-});
-
+app.get('/', (req, res) => res.send('Curate API running'));
 module.exports = app;
