7aeb02fb26884c66f5d8d96440a1c8d2e6f36679
IsiwaraKumarage8 <isiwarakumarage6@gmail.com>
2026-08-23 01:33:33 +0530

    update app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from './components/Navbar'
 import Landing from './pages/Landing'
 import Board from './components/Board'
 import Dashboard from './pages/Dashboard'
+import Login from './pages/Login'
+import Register from './pages/Register'
 import { ToastProvider } from './context/ToastContext'
 import { AuthProvider } from './context/AuthContext'
 import Toast from './components/Toast'
@@ -21,6 +23,8 @@ function App() {
           )}
           {view === 'board' && <Board />}
           {view === 'dashboard' && <Dashboard />}
+          {view === 'login' && <Login onSuccess={() => setView('board')} />}
+          {view === 'register' && <Register onSuccess={() => setView('board')} />}
           <Toast />
         </div>
       </ToastProvider>
