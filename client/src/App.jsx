4e07e565fa04d4f85b660e2c816ffe1611faf096
IsiwaraKumarage8 <isiwarakumarage6@gmail.com>
2026-08-22 11:15:32 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,28 +5,26 @@ import Landing from './pages/Landing'
 import Board from './components/Board'
 import Dashboard from './pages/Dashboard'
 import { ToastProvider } from './context/ToastContext'
+import { AuthProvider } from './context/AuthContext'
 import Toast from './components/Toast'
 import { useState } from 'react'
 
 function App() {
   const [view, setView] = useState('landing')
-
   return (
-    <ToastProvider>
-      <div className="app">
-        <Navbar currentView={view} onNavigate={setView} />
-
-        {view === 'landing' && (
-          <Landing onGetStarted={() => setView('board')} />
-        )}
-
-        {view === 'board' && <Board />}
-
-        {view === 'dashboard' && <Dashboard />}
-
-        <Toast />
-      </div>
-    </ToastProvider>
+    <AuthProvider>
+      <ToastProvider>
+        <div className="app">
+          <Navbar currentView={view} onNavigate={setView} />
+          {view === 'landing' && (
+            <Landing onGetStarted={() => setView('board')} />
+          )}
+          {view === 'board' && <Board />}
+          {view === 'dashboard' && <Dashboard />}
+          <Toast />
+        </div>
+      </ToastProvider>
+    </AuthProvider>
   )
 }
 
