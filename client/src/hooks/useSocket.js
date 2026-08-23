1ac8342590159c7f31cf5df5abfedf33a9fdf6b2
tharukshinuwanthika28-glitch <tharukshinuwanthika28@gmail.com>
2026-08-23 13:48:56 +0530

    feat: add Socket.io client real-time board updates

---
+++ client/src/hooks/useSocket.js
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { io } from 'socket.io-client';
+
+const SOCKET_URL = 'http://localhost:5000';
+
+export function useSocket(onSampleUpdated, showToast) {
+  useEffect(() => {
+    const socket = io(SOCKET_URL);
+
+    socket.on('connect', () => {
+      console.log('Socket connected:', socket.id);
+    });
+
+    socket.on('sampleUpdated', (updatedSample) => {
+      console.log('Live sample update received:', updatedSample);
+
+      if (onSampleUpdated) {
+        onSampleUpdated(updatedSample);
+      }
+
+      if (showToast) {
+        showToast(
+          'A sample was updated by another user.',
+          'success'
+        );
+      }
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Socket disconnected');
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [onSampleUpdated, showToast]);
+}
