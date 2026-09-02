144bbb6e9dc18d35bd362f6ef1134938f7f79fbb
IsiwaraKumarage8 <isiwarakumarage6@gmail.com>
2026-09-03 01:01:25 +0530

    create models/Board.js file

---
+++ server/models/Board.js
@@ -0,0 +1,22 @@
+@'
+const mongoose = require('mongoose');
+
+const boardSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, trim: true },
+    description: { type: String, default: '' },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+module.exports = mongoose.model('Board', boardSchema);
+'@ | Set-Content -Path models\Board.js -Encoding utf8
